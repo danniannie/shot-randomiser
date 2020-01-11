485dc5eb70ceeae041d8bdfaa97af880d6b63fb7
danniannie <dannielle.young@hotmail.com>
2020-01-11 22:28:57 +0000

    updated styling at functionality

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
-//const addButton = document.getElementById("add");
 const spiritForm = document.getElementById("spirit-form");
 
 const addDrink = event => {
   event.preventDefault();
-  const item = document.createElement("li");
+  if (document.querySelectorAll("p.drink").length === 0) {
+    const title = document.createElement("h3");
+    title.innerText = "The Bar";
+    const referenceNode = document.getElementById("list");
+    const parentNode = document.getElementById("the-bar");
+    parentNode.insertBefore(title, referenceNode);
+  }
+  const item = document.createElement("p");
+  item.setAttribute("class", "drink");
   let drink = document.getElementById("spirit-input").value;
   item.innerText = drink;
   document.getElementById("list").appendChild(item);
   document.getElementById("spirit-form").reset();
 };
 
-//addButton.addEventListener("click", addDrink);
 spiritForm.addEventListener("submit", addDrink);
 
 const randomButton = document.getElementById("random");
 
 const randomShot = event => {
   event.preventDefault();
-  const drinks = document.querySelectorAll("li");
+  const drinks = document.querySelectorAll("p.drink");
   const drinksArray = [];
   drinks.forEach(drink => {
     drinksArray.push(drink.innerText);
@@ -27,7 +33,14 @@ const randomShot = event => {
     drinksArray[Math.floor(Math.random() * Math.floor(drinksArray.length))];
 
   const currentP = document.getElementById("drink-selector");
-  currentP.innerText = chosenDrink || "Add a drink to be picked!";
+  currentP.innerText = chosenDrink
+    ? `Drink >>> ${chosenDrink}`
+    : "Add a drink to be picked!";
+  if (currentP.className == "selected") {
+    currentP.className = "purple";
+  } else {
+    currentP.className = "selected";
+  }
 };
 
 randomButton.addEventListener("click", randomShot);
